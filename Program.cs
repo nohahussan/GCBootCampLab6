ac4ce60cd0c05c69184c53f7d794b83862004122
Noha <noha.hussan@gmail.com>
2019-01-17 16:09:34 -0500

    Submitting Lab6

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+namespace Lab6
+{                           //Application that simulates dice rolling 
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+                Console.WriteLine("Welcome to the Grand Circus Casino! Roll the dice?(y/n)");
+                string starting = Console.ReadLine().ToLower();
+                if(starting == "n")
+                {
+                    return; // If the user doesn't want to play the game the program will terminate
+                }
+                //If the user would like to run the game again
+                Boolean repeat = true;
+                int counter = 0; //count how many times the user will play the game
+
+            do
+            {
+                counter++;// The user started playing
+                Console.WriteLine("How many sides should each die have?");
+                int numSides = int.Parse(Console.ReadLine());
+                Console.WriteLine("Roll {0}: ", counter);
+                RandomNumberGenerator(numSides);
+                Continue(repeat);
+
+            } while (repeat);
+        }
+
+        public static Boolean Continue(Boolean repeat) 
+        {
+            Console.WriteLine("Roll again?(y/n)");
+            string userChoice = Console.ReadLine().ToLower();
+            repeat = (userChoice == "y") ? true : false;
+            return repeat;
+        }
+
+        public static void RandomNumberGenerator (int num)
+        {
+            Random rnd = new Random();
+            int numGen1 = rnd.Next(1, num+1);
+            int numGen2 = rnd.Next(1, num+1);
+            Console.WriteLine(numGen1 + "\n" + numGen2);
+
+        }
+
+    }
+}
